dcff212f739e15a6ff6932330cee9f30fdb69112
Emi-H106 <emi.hatano@yahoo.com>
2025-09-10 10:59:24 +0200

    add interface Directors

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -23,5 +23,19 @@ const teacher2: Teacher = {
   contract: true,
 };
 
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+};
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: true,
+  location: "San Francisco",
+  numberOfReports: 10,
+};
+
 console.log(teacher1);
 console.log(teacher2);
+console.log(director1);

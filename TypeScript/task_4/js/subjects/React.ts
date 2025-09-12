f1dba130348824af3b4381c6bce4eb5de90f7bf4
Emi-H106 <emi.hatano@yahoo.com>
2025-09-12 12:16:20 +0200

    add task_4 & task_5

---
+++ TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,15 @@
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      if (this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
